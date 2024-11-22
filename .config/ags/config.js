a84730ed3c78fa53c76cf052a19be4d5d63211d3
Abdullah Muhamed <cdumb@proton.me>
2024-11-22 19:54:03 +0200

    .

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -7,15 +7,15 @@ import App from "resource:///com/github/Aylur/ags/app.js";
 // Stuff
 import userOptions from "./modules/.configuration/user_options.js";
 import {
-  firstRunWelcome,
-  startBatteryWarningService,
+    firstRunWelcome,
+    startBatteryWarningService,
 } from "./services/messages.js";
 import { startAutoDarkModeService } from "./services/darkmode.js";
 // Widgets
 import {
-  Bar,
-  BarCornerTopleft,
-  BarCornerTopright,
+    Bar,
+    BarCornerTopleft,
+    BarCornerTopright,
 } from "./modules/bar/main.js";
 import Cheatsheet from "./modules/cheatsheet/main.js";
 // import DesktopBackground from './modules/desktopbackground/main.js';
@@ -31,14 +31,14 @@ import SideRight from "./modules/sideright/main.js";
 import { COMPILED_STYLE_DIR } from "./init.js";
 
 const range = (length, start = 1) =>
-  Array.from({ length }, (_, i) => i + start);
+    Array.from({ length }, (_, i) => i + start);
 function forMonitors(widget) {
-  const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
-  return range(n, 0).map(widget).flat(1);
+    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+    return range(n, 0).map(widget).flat(1);
 }
 function forMonitorsAsync(widget) {
-  const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
-  return range(n, 0).forEach((n) => widget(n).catch(print));
+    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+    return range(n, 0).forEach((n) => widget(n).catch(print));
 }
 
 // Start stuff
@@ -48,40 +48,40 @@ firstRunWelcome().catch(print);
 startBatteryWarningService().catch(print);
 
 const Windows = () => [
-  // forMonitors(DesktopBackground),
-  // forMonitors(Crosshair),
-  // Overview(),
-  forMonitors(Indicator),
-  forMonitors(Cheatsheet),
-  SideLeft(),
-  SideRight(),
-  //forMonitors(Osk),
-  forMonitors(Session),
-  ...(userOptions.asyncGet().dock.enabled ? [forMonitors(Dock)] : []),
-  ...(userOptions.asyncGet().appearance.fakeScreenRounding !== 0
-    ? [
-        forMonitors((id) => Corner(id, "top left", true)),
-        forMonitors((id) => Corner(id, "top right", true)),
-        //forMonitors((id) => Corner(id, 'bottom left', true)),
-        //forMonitors((id) => Corner(id, 'bottom right', true)),
-      ]
-    : []),
-  ...(userOptions.asyncGet().appearance.barRoundCorners
-    ? [forMonitors(BarCornerTopleft), forMonitors(BarCornerTopright)]
-    : []),
+    // forMonitors(DesktopBackground),
+    // forMonitors(Crosshair),
+    // Overview(),
+    forMonitors(Indicator),
+    forMonitors(Cheatsheet),
+    SideLeft(),
+    SideRight(),
+    //forMonitors(Osk),
+    forMonitors(Session),
+    ...(userOptions.asyncGet().dock.enabled ? [forMonitors(Dock)] : []),
+    ...(userOptions.asyncGet().appearance.fakeScreenRounding !== 0
+        ? [
+              forMonitors((id) => Corner(id, "top left", true)),
+              forMonitors((id) => Corner(id, "top right", true)),
+              forMonitors((id) => Corner(id, "bottom left", true)),
+              forMonitors((id) => Corner(id, "bottom right", true)),
+          ]
+        : []),
+    ...(userOptions.asyncGet().appearance.barRoundCorners
+        ? [forMonitors(BarCornerTopleft), forMonitors(BarCornerTopright)]
+        : []),
 ];
 
 const CLOSE_ANIM_TIME = 180; // Longer than actual anim time to make sure widgets animate fully
 const closeWindowDelays = {}; // For animations
 for (let i = 0; i < (Gdk.Display.get_default()?.get_n_monitors() || 1); i++) {
-  closeWindowDelays[`osk${i}`] = CLOSE_ANIM_TIME;
+    closeWindowDelays[`osk${i}`] = CLOSE_ANIM_TIME;
 }
 
 App.config({
-  css: `${COMPILED_STYLE_DIR}/style.css`,
-  stackTraceOnError: true,
-  closeWindowDelay: closeWindowDelays,
-  windows: Windows().flat(1),
+    css: `${COMPILED_STYLE_DIR}/style.css`,
+    stackTraceOnError: true,
+    closeWindowDelay: closeWindowDelays,
+    windows: Windows().flat(1),
 });
 
 // Stuff that don't need to be toggled. And they're async so ugh...
