565f18040de98678ba38fcbfac1acaa8d76f4e9f
Abdullah Muhamed <cdumb@proton.me>
2024-11-28 19:38:53 +0200

    /

--- .config/ags/modules/sideright/sideright.js
+++ .config/ags/modules/sideright/sideright.js
@@ -284,7 +284,7 @@ export default () =>
             children: [sidebarOptionsStack],
           }),
           // ModuleCalendar(),
-          WeatherWidget(),
+          // WeatherWidget(),
         ],
       }),
     ],
