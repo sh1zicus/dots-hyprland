c52121a29daab73c7f5c29c1d322923a5c39f95b
sh1zicus <87767572+sh1zicus@users.noreply.github.com>
2025-05-01 22:21:31 +0700

    [CROSSHAIR] update

--- .config/ags/modules/crosshair/main.js
+++ .config/ags/modules/crosshair/main.js
@@ -6,16 +6,33 @@ export default (monitor = 0, ) => {
         monitor,
         name: `crosshair${monitor}`,
         layer: 'overlay',
-        exclusivity: 'ignore',
+        type: 'dock',
+        exclusivity: 'exclusive',
         visible: false,
-        child: Widget.Icon({
-            icon: 'crosshair-symbolic',
-            css: `
-                font-size: ${userOptions.asyncGet().gaming.crosshair.size}px;
-                color: ${userOptions.asyncGet().gaming.crosshair.color};
-            `,
+        className: 'crosshair',
+        child: Widget.Box({
+            vertical: true,
+            child: Widget.Box({
+                vertical: true,
+                child: Widget.Icon({
+                    icon: 'crosshair-symbolic',
+                    css: `
+                        font-size: ${userOptions.asyncGet().gaming.crosshair.size}px;
+                        color: ${userOptions.asyncGet().gaming.crosshair.color};
+                    `,
+                }),
+                css: `
+                    margin-bottom: 37px;
+                `
+            }),
         }),
-        setup: enableClickthrough,
+        setup: (window) => {
+            enableClickthrough(window);
+            // Make the window ignore pointer events
+            window.set_property('can-focus', false);
+            window.set_property('can-target', false);
+            window.set_property('accepts-focus', false);
+            window.set_property('focus-on-map', false);
+        },
     });
 }
-
